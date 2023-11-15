ce9d706f9504c4385682c6f77c5e8c751abf0631
Monky518 <zoeyrizor@gmail.com>
2023-11-15 12:56:25 -0500

    Finished but no screenshots yet

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,82 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main
 {
+    public static ArrayList<String> myArrList = new ArrayList<>();
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        String userInput = "";
+        String userInput;
         boolean done = false;
 
         do
         {
-            showList(); // do I need this to show every time?
             showMenu();
-            if (userInput.matches("Aa"))
+            userInput = SafeInput.getRegExString(scan, "What would you like to do?", "[AaDdPpQq]");
+            if (userInput.equalsIgnoreCase("A"))
                 add();
-            else if (userInput.matches("Dd"))
+            else if (userInput.equalsIgnoreCase("D"))
                 delete();
-            else if (userInput.matches("Pp"))
+            else if (userInput.equalsIgnoreCase("P"))
                 print();
+            else if (userInput.equalsIgnoreCase("Q"))
+                done = quit();
             else
-                quit();
+                System.out.println("\nERROR: This should not be an options\n");
         } while(!done);
     }
 
     public static void showMenu()
     {
-        // show the entire menu options
-    }
-
-    public static void showList()
-    {
-        // display the entire list
+        System.out.println("----------------------------------------");
+        System.out.println("A – Add an item to the list");
+        System.out.println("D – Delete an item from the list");
+        System.out.println("P – Print (i.e. display) the list");
+        System.out.println("Q – Quit the program");
+        System.out.println("----------------------------------------");
     }
 
     public static void add()
     {
-        // ask what to add and where
+        String userInput = SafeInput.getNonZeroLenString(scan, "What would you like to add?");
+        int userInt = 0;
+        if (!myArrList.isEmpty())
+        {
+            print(); // shows the current list to set placement
+            userInt = SafeInput.getRangedInt(scan, "Where would you like to add " + userInput, 0, myArrList.size() - 1);
+        }
+        myArrList.add(userInt, userInput);
     }
 
     public static void delete()
     {
-        // You need to display a numbered version of the list to allow users to pick list elements for deletion
-        // Here the user looks at the display and then indicates the item to delete by the number
-
-        // check for empty list
-        // ask what to delete
+        print(); // shows the current list to delete specific item
+        if (myArrList.isEmpty())
+            System.out.println("\nERROR: Your list is empty right now\n");
+        else
+        {
+            int userInt = SafeInput.getRangedInt(scan, "Which item do you want to delete", 0, myArrList.size() - 1);
+            myArrList.remove(userInt);
+        }
     }
 
     public static void print()
     {
-        // check for empty list
-        // ask what to print
+        if (myArrList.isEmpty())
+            System.out.println("\nERROR: Your list is empty right now\n");
+        else
+        {
+            for (int i = 0; i < myArrList.size(); i++)
+            {
+                System.out.println(i + " - " + myArrList.get(i));
+            }
+        }
     }
 
-    public static void quit()
+    public static boolean quit()
     {
-        // ask to confirm
-        // stop the function
+        return SafeInput.getYNConfirm(scan, "Are you sure you want to quit?");
     }
 }
